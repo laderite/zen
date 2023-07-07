2175784b08dd4f7040779f8bfe293a96c0027a6f
laderite <78981916+laderite@users.noreply.github.com>
2023-07-06 22:08:33 -0700

    Update UniversalBuilder.lua

--- modules/UniversalBuilder.lua
+++ modules/UniversalBuilder.lua
@@ -70,12 +70,53 @@ local UniversalBuilder = {} do
         PlayerSection:AddToggle('HipHeight', {Text = 'Hip Height', Default = false}):AddKeyPicker('HipheightKeybind', { Default = '', SyncToggleState = true, NoUI = false, Text = 'Hip Height'})
         PlayerSection:AddSlider('HipHeightSlider', { Text = 'Value', Default = initHipHeight, Min = initHipHeight, Max = 200, Rounding = 0, Compact = true})
 
+        PlayerSection:AddDivider()
+
+        PlayerSection:AddToggle('InfJump', {Text = 'Infinite Jump', Default = false}):AddKeyPicker('InfJumpKeybind', { Default = '', SyncToggleState = true, NoUI = false, Text = 'Infinite Jump'})
+        PlayerSection:AddToggle('Noclip', {Text = 'Noclip', Default = false}):AddKeyPicker('NoclipKeybind', { Default = '', SyncToggleState = true, NoUI = false, Text = 'Noclip'})
+
         othersection:AddSlider('FOVSlider', { Text = 'Camera FOV', Default = initFOV, Min = 1, Max = 120, Rounding = 0, Compact = false, Callback = function(v)
             if workspace:FindFirstChild('Camera') then workspace.Camera.FieldOfView = v end
         end})
 
         othersection:AddButton('Execute Infinite Yield', function()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end)
+
+        othersection:AddButton('FPS Boost', function()
+
+            local descendants = game:GetDescendants()
+            for i = 1, #descendants do
+                local v = descendants[i]
+                if v.ClassName == "Texture" then
+                    v.Texture = ""
+                elseif v:IsA("BasePart") and v.Material ~= Enum.Material.Wood then
+                    v.Material = Enum.Material.Wood
+                end
+            end
+
+        end)
+
+        othersection:AddButton('Rejoin Server', function()
+            self.Library:Notify('rejoining the same server..')
+            game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+        end)
+
+        othersection:AddButton('Server Hop', function()
+            local s, e = pcall(function()
+                local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"))
+                for i,v in pairs(Servers.data) do
+                    if v.playing ~= v.maxPlayers then
+                        game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
+                    end
+                end
+            end)
+
+            if s then
+                self.Library:Notify('server hopping..')
+            else
+                self.Library:Notify('error occured while server hopping: ' .. tostring(e))
+            end
         end)
 
         UniversalBuilder:Init()
