6e5ea5c1230d6e324882f102121b7a09080f2dfb
laderite <78981916+laderite@users.noreply.github.com>
2023-07-06 20:45:03 -0700

    Update UniversalBuilder.lua

--- modules/UniversalBuilder.lua
+++ modules/UniversalBuilder.lua
@@ -1,66 +1,85 @@
 Players = game:GetService('Players')
 player = Players.LocalPlayer
 
+local initWalkSpeed = player.Character:FindFirstChildWhichIsA('Humanoid').WalkSpeed
+local initJumpPower = player.Character:FindFirstChildWhichIsA('Humanoid').JumpPower
+local initHipHeight = player.Character:FindFirstChildWhichIsA('Humanoid').HipHeight
+local initFOV = math.floor(workspace:FindFirstChildWhichIsA('Camera').FieldOfView)
+
 local UniversalBuilder = {} do
 
-    function UniversalBuilder.Build(tab)
+    function UniversalBuilder:SetLibrary(library)
+		self.Library = library
+	end
 
-        local initWalkSpeed = player.Character:FindFirstChildWhichIsA('Humanoid').Walkspeed
-        local initJumpPower = player.Character:FindFirstChildWhichIsA('Humanoid').JumpPower
-        local initFOV = workspace:FindFirstChildWhichIsA('Camera').FieldOfView
-
-        local WalkspeedSection = tab:AddRightGroupbox('Walk Speed')
-        local JumppowerSection = tab:AddRightGroupbox('Jump Power')
-        local HipHeightSection = tab:AddRightGroupbox('Hip Height')
-        local othersection = tab:AddRightGroupbox('Other Features')
-
-        WalkspeedSection:AddDivider()
-        JumppowerSection:AddDivider()
-        HipHeightSection:AddDivider()
-        othersection:AddDivider()
-
-        WalkspeedSection:AddToggle('Walkspeed', {Text = 'Walk Speed', Default = false})
-        WalkspeedSection:AddSlider('WalkspeedSlider', { Text = '', Default = initWalkSpeed, Min = initWalkSpeed, Max = 200, Rounding = 0, Compact = true})
-
-        JumppowerSection:AddToggle('Jumppower', {Text = 'Jump Power', Default = false})
-        JumppowerSection:AddSlider('JumppowerSlider', { Text = '', Default = initJumpPower, Min = initJumpPower, Max = 200, Rounding = 0, Compact = true})
-
-        HipHeightSection:AddToggle('HipHeight', {Text = 'Hip Height', Default = false})
-        HipHeightSection:AddSlider('HipHeightSlider', { Text = '', Default = 0, Min = 0, Max = 200, Rounding = 0, Compact = true})
-
-        othersection:AddButton('Infinite Yield', function()
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    function UniversalBuilder:Init()
+        
+        task.spawn(function()
+            while task.wait() do
+                if Toggles.Jumppower.Value and (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
+                    player.Character:FindFirstChildWhichIsA('Humanoid').JumpPower = Options.JumppowerSlider.Value
+                else
+                    if (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
+                        player.Character:FindFirstChildWhichIsA('Humanoid').JumpPower = initJumpPower
+                    end
+                end
+            end
         end)
+        
+        task.spawn(function()
+            while task.wait() do
+                if Toggles.Walkspeed.Value and (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
+                    player.Character:FindFirstChildWhichIsA('Humanoid').WalkSpeed = Options.WalkspeedSlider.Value
+                else
+                    if (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
+                        player.Character:FindFirstChildWhichIsA('Humanoid').WalkSpeed = initWalkSpeed
+                    end
+                end
+            end
+        end)
+        
+        task.spawn(function()
+            while task.wait() do
+                if Toggles.HipHeight.Value and (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
+                    player.Character:FindFirstChildWhichIsA('Humanoid').HipHeight = Options.HipHeightSlider.Value
+                else
+                    if (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
+                        player.Character:FindFirstChildWhichIsA('Humanoid').HipHeight = initHipHeight
+                    end
+                end
+            end
+        end)
+
+    end
+
+    function UniversalBuilder:Build(tab)
+
+        local PlayerSection = tab:AddLeftGroupbox('Local Player')
+        local othersection = tab:AddLeftGroupbox('Other Features')
+
+        PlayerSection:AddToggle('Walkspeed', {Text = 'Walk Speed', Default = false}):AddKeyPicker('WalkspeedKeybind', { Default = '', SyncToggleState = true, NoUI = false, Text = 'Walk Speed'})
+        PlayerSection:AddSlider('WalkspeedSlider', { Text = 'Value', Default = initWalkSpeed, Min = initWalkSpeed, Max = 200, Rounding = 0, Compact = true})
+
+        PlayerSection:AddDivider()
+
+        PlayerSection:AddToggle('Jumppower', {Text = 'Jump Power', Default = false}):AddKeyPicker('JumppowerKeybind', { Default = '', SyncToggleState = true, NoUI = false, Text = 'Jump Power'})
+        PlayerSection:AddSlider('JumppowerSlider', { Text = 'Value', Default = initJumpPower, Min = initWalkSpeed, Max = 200, Rounding = 0, Compact = true})
+
+        PlayerSection:AddDivider()
+
+        PlayerSection:AddToggle('HipHeight', {Text = 'Hip Height', Default = false}):AddKeyPicker('HipheightKeybind', { Default = '', SyncToggleState = true, NoUI = false, Text = 'Hip Height'})
+        PlayerSection:AddSlider('HipHeightSlider', { Text = 'Value', Default = initHipHeight, Min = initHipHeight, Max = 200, Rounding = 0, Compact = true})
 
         othersection:AddSlider('FOVSlider', { Text = 'Camera FOV', Default = initFOV, Min = 1, Max = 120, Rounding = 0, Compact = false, Callback = function(v)
             if workspace:FindFirstChild('Camera') then workspace.Camera.FieldOfView = v end
         end})
-    end
 
+        othersection:AddButton('Execute Infinite Yield', function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end)
+
+        UniversalBuilder:Init()
+    end
 end
-
-task.spawn(function()
-    while task.wait() and Toggles.Jumppower.Value do
-        if (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
-            player.Character:FindFirstChildWhichIsA('Humanoid').JumpPower = Options.JumppowerSlider.Value
-        end
-    end
-end)
-
-task.spawn(function()
-    while task.wait() and Toggles.Walkspeed.Value do
-        if (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
-            player.Character:FindFirstChildWhichIsA('Humanoid').Walkspeed = Options.WalkspeedSlider.Value
-        end
-    end
-end)
-
-task.spawn(function()
-    while task.wait() and Toggles.HipHeight.Value do
-        if (player.Character and player.Character:FindFirstChildWhichIsA('Humanoid')) then
-            player.Character:FindFirstChildWhichIsA('Humanoid').HipHeight = Options.HipHeightSlider.Value
-        end
-    end
-end)
 
 return UniversalBuilder
